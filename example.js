d1998c2f13d2749296e488c5dcb282065a831c07
shanshan_pan <shanshan_pan@kingdee.com>
2018-09-03 18:06:31 +0800

    注释

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 // 利用函数式编程实现 找出一句英文中 单词最长的词的长度的功能
 
-const pipe = (...rest) => {
+const pipe = (...rest) => { // 将函数按按照顺序摆放
     return function(x){
         let y= x
         for(func of rest){
@@ -10,11 +10,11 @@ const pipe = (...rest) => {
     }
 }
 
-const split = s => s.split(' ')
+const split = s => s.split(' ') // 按照' '分割字符串
 
-const length = w => w.length
+const length = w => w.length // 获取字符串长度
 
-const map = f => {
+const map = f => { // 柯里化map
     return function(arr){
         return arr.map(function(item){
             return f(item)
@@ -22,28 +22,35 @@ const map = f => {
     }
 }
 
-const reduce= f =>{
+const reduce= f =>{ // 柯里化reduce
     return function(arr){
         return arr.reduce(f)
     }
 }
 
-const max = (a, b) => a>b ? a : b
+const max = (a, b) => a>b ? a : b // 定义max函数
 
-const replace = (a,b) => {
-    return (x)=>{
-        return x.replace(a,b)
+const replace = (a) => { // 柯里化replace
+    return function(b){
+        return (x)=>{
+            return x.replace(a,b)
+        }
     }
 } 
 
-const filterSource = replace('.','')
+const filterSource = replace('.')('') // 定义去掉句子中影响长度的'.'的函数
 
-const getLengthArr = map(length)
+const getLengthArr = map(length) // 定义将数组元素替换为元素长度的函数
 
-const findBiggestNumber = reduce(max)
+const findBiggestNumber = reduce(max) // 定义寻找最大值的函数
 
-const lengthOfLongestWord = pipe(filterSource,split,getLengthArr,findBiggestNumber)
+const lengthOfLongestWord = pipe(filterSource,split,getLengthArr,findBiggestNumber) // 定义找出一句英文中单词最长的词的长度的函数
 
-const str = 'Get the length of the longest word from the sentence.'
+const str = 'Get the length of the longest word from the sentence.' // x
 
-lengthOfLongestWord(str)
+lengthOfLongestWord(str) // y = f(x)
+
+// x = 'Get the length of the longest word from the sentence.'  
+// f = 定义找出一句英文中单词最长的词的长度
+// y = f(x)
+// y = 8
